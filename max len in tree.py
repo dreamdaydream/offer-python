8e938cd0ae4a5d5613ab51f91dce9f8a501ecaf8
fairylyn <fll_005@163.com>
2018-08-30 15:46:56 +0800

    Update max len in tree.py

--- max len in tree.py
+++ max len in tree.py
@@ -1,4 +1,7 @@
 # -*- coding:utf-8 -*-
+#二叉树中最远的两结点距离
+#若左右子树均存在，则最远距离为左右字数的深度和
+#若只有左/右子树，则最远距离为左子树的最远距离
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -12,6 +15,7 @@ class Solution:
         left_len = self.GetMaxlen(pRoot.left)
         right_len = self.GetMaxlen(pRoot.right)
         max_len = max(self.Deep(pRoot.left)+self.Deep(pRoot.right), left_len, right_len)
+ 
         return max_len
 
     def Deep(self,pRoot):
